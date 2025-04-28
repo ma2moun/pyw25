155538bb2d24db1a190bd84c577c492b950963b3
ma2moun <dr.maamoun@outlook.com>
2025-04-28 23:25:23 +0400

    Update day1.py

--- day1.py
+++ day1.py
@@ -6,5 +6,3 @@ name=input("Enter your name: ")
 print(f"Dividing {x} over {y} results {x//y} and a remainder of {x%y}")
 print("Dividing",x, "over", y, "results", x//y, "and a remainder of", x%y)
 
-# student.dcec
-# mtc@2020
