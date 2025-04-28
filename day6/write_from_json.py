76104c84e1878c68d3e4d748cd4e08ab625a2b7b
ma2moun <dr.maamoun@outlook.com>
2025-04-28 23:57:40 +0400

    Update write_from_json.py

--- day6/write_from_json.py
+++ day6/write_from_json.py
@@ -3,15 +3,19 @@ import json
 # JSON data
 x =  '{ "name":"Ahmed", "age":30, "city":"Muscat"}'
 
-#or read from a file:
-""" json_file=open("out.json", "r")
-x=json_file.read() """
-
 # parse x using loads:
 y = json.loads(x)
 
+
+# If reading from a json file:
+with open("day6\\readthis.json") as f:
+    y2=json.load(f)
+
+
 # the result is a Python dictionary:
 print(y["age"])
-
 # print the dictionary
-print(y)
+print(f"Data from the x JSON: {y}")
+
+# or the data from the file:
+print(f"Data from the file: {y2}")
